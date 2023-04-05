bf9ac033220c997d6b370182ef7b32594a5f7f44
Rodrigo <rodrigogrigoleto@gmail.com>
2023-04-05 17:49:46 -0300

    Primeiro Commit

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApiConsulta.Models;
+
+namespace PrimeiraApiConsulta.Controllers
+{
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet]
+        public List<Pessoa> ObterPessoas()
+        {
+            Pessoa pessoa = new Pessoa();
+            return pessoa.ObterPessoas();
+        }
+
+        [HttpGet]
+        public Pessoa ObterPessoaById(int id)
+        {
+            Pessoa pessoa = new Pessoa();
+            return pessoa.ObterPessoaById(id);
+        }
+
+        [HttpGet("{controller}/{action}/{id}/{idTeste}")]
+        public Pessoa ObterPessoaById([FromRoute]int id, [FromRoute] int idTeste)
+        {
+            Pessoa pessoa = new Pessoa();
+            return pessoa.ObterPessoaById(id);
+        }
+
+        [HttpGet("{controller}/{action}/{id}")]
+        public Pessoa ObterPessoaById([FromRoute] int id, [FromQuery] string idTeste)
+        {
+            Pessoa pessoa = new Pessoa();
+            return pessoa.ObterPessoaById(id);
+        }
+
+
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿namespace PrimeiraApiConsulta.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Cep { get; set; }
+        public string Cpf { get; set; }
+
+        public Pessoa ObterPessoaById(int idPessoa)
+        {
+            // Obtendo a lista das pessoas
+            List<Pessoa> listaPessoas = ObterPessoas();
+
+            // Procurando por uma pessoa através do Id
+            return listaPessoas.Find(pessoaDaLista =>
+                pessoaDaLista.Id == idPessoa);
+        }
+
+        public List<Pessoa> ObterPessoas()
+        {
+            List<Pessoa> listaPessoas = new List<Pessoa>()
+            {
+                new Pessoa() {
+                    Id = 1,
+                    Nome = "Pessoa A",
+                    Endereco = "Endereco Pessoa A",
+                    Cpf = "123456",
+                    Cep = "29780000"
+                },
+                new Pessoa() {
+                    Id = 2,
+                    Nome = "Pessoa B",
+                    Endereco = "Endereco Pessoa B",
+                    Cpf = "654987",
+                    Cep = "29000000"
+                },
+                new Pessoa() {
+                    Id = 3,
+                    Nome = "Pessoa C",
+                    Endereco = "Endereco Pessoa C",
+                    Cpf = "654987",
+                    Cep = "29000000"
+                },
+            };
+
+            return listaPessoas;
+        }
+    }
+}
